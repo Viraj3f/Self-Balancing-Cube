5d1e422ce5eb3767244ce54bc56d590453a38d6f
Viraj Bangari <Viraj3f@gmail.com>
2018-09-28 15:39:16 -0400

    Add control system parameters to matlab

--- simulations/model_parameters.m
+++ simulations/model_parameters.m
@@ -6,6 +6,7 @@ mw = 0.204;
 Ib = 3.34e-3;
 Iw = 0.57e-3;
 Cb = 1.02e-3;
+
 Cw = 0.05e-3;
 g = 9.81;
 Km = 25.1e-3;
@@ -19,3 +20,7 @@ b0 = -a0;
 b1 = (Ib + Iw + mw * l^2) / (Iw * (Ib + mw * l^2));
 b2 = -a2;
 b3 = -Cw * b1;
+
+% Control system parameters
+maxDCCurrent = 10;
+sampleTime = 0.01;
