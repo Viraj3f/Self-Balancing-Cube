3fad81e3dd0e57284e050196a0913fc702b311a6
Viraj Bangari <Viraj3f@gmail.com>
2018-10-03 17:42:44 -0400

    Use more correct model values

--- simulations/model_parameters.m
+++ simulations/model_parameters.m
@@ -1,17 +1,27 @@
-    % Model parameters
+%% Model parameters
+g = 9.81;
 l = 0.085;
 lb = 0.075;
-mb = 0.419;
-mw = 0.204;
-Ib = 3.34e-3;
-Iw = 0.57e-3;
+mb = 0.123;  % kg
+mw = 0.152;  % kg
+Ib = 4.9e-3;  % m^-2
+Iw = 2.7e-3;  % m^-2
 Cb = 1.02e-3;
-
 Cw = 0.05e-3;
-g = 9.81;
 Km = 25.1e-3;
 
-% Diff Eq coefficients
+%% Old
+% l = 0.085;
+% lb = 0.075;
+% mb = 0.419;
+% mw = 0.204;
+% Ib = 3.34e-3;
+% Iw = 0.57e-3;
+% Cb = 1.02e-3;
+% Cw = 0.05e-3;
+% Km = 25.1e-3;
+
+%% Diff Eq coefficients
 a0 = (mb * lb + mw * l) * g / (Ib + mw * l^2);
 a1 = -1 / (Ib + mw * l^2);
 a2 = -Cb / (Ib + mw * l^2);
@@ -21,8 +31,6 @@ b1 = (Ib + Iw + mw * l^2) / (Iw * (Ib + mw * l^2));
 b2 = -a2;
 b3 = -Cw * b1;
 
-% Control system parameters
+%% Control system parameters
 maxDCCurrent = 10;
 sampleTime = 0.01;
-
-sim('PID_simulation', 'TimeOut', 10)
