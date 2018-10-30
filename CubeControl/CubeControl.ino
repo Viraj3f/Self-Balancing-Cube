85116ba97c609187c66639c9e9a6d014c65d9fb9
Hugh Morison <14hm40@caslab.queensu.ca>
2018-10-30 11:24:41 -0400

    Initial IMU code

--- CubeControl/CubeControl.ino
+++ CubeControl/CubeControl.ino
@@ -1,71 +1,82 @@
-#include <Servo.h> //Using servo library to control ESC
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-
 /*
    Connections
    ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
+   --From IMU--
+   Connect SCL to D21
+   Connect SDA to D20
    Connect VDD to 3.3V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
+   Connect GROUND to GND
+   --From ESC--
+   Connect ESC to D9 and GND
 */
 
-/* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (100)
+#include <Adafruit_BNO055.h>
+#include <Servo.h> //Using servo library to control ESC
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
-
-Servo esc; //Creating a servo class with name as esc
-int serialVal = 1000;
-
-bool firstLoop = true;
-int startAngle = 0;
+Servo esc;
+const int angleBound = 50;
+double angleOffset = 0;
 
 void setup() {
-  /*ESC stuff*/
-  esc.attach(9); //Specify the esc signal pin,Here as D8
-  esc.writeMicroseconds(1500);
-  delay(2000);
-  Serial.begin(9600);
-
-  /*BNO stuff*/
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-  delay(1000);
-  bno.setExtCrystalUse(true);
-  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+    Serial.begin(9600);
+    
+    // Esc setup
+    esc.attach(9);
+    esc.writeMicroseconds(1500);
+    delay(2000);
+    Serial.println("Esc is setup");
+    
+    // BNO setup
+    if(!bno.begin()) {
+        printErrorAndExit("Could not connect to BNO.");
+    }
+    delay(1000);
+    Serial.println("BNO is attached");
+    bno.setExtCrystalUse(true);
+    
+    delay(2000);
+    double initialAngle = getAngleFromIMU(bno);
+    if (initialAngle <= 45){
+        angleOffset = 45.0 + initialAngle;
+    }
+    else {
+        angleOffset = initialAngle - 45.0;
+    }
+    Serial.println("BNO is calibrated with offset: " + String(angleOffset));
 }
 
 
 void loop() {
+    double angle = getAngleFromIMU(bno);
+    if (fabs(angle) > angleBound) {
+      printErrorAndExit("Angle is past max angle. Current angle: " + String(angle) + " Angle bound: " + String(angleBound));
+    }
+
+    int escValue = getEscValFromAngle(angle); 
+    Serial.println(angle);
+    
+    //esc.writeMicroseconds(escValue);
+}
+
+double getAngleFromIMU(Adafruit_BNO055 &bno){
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  if (firstLoop == true){
-    startAngle = euler.x();
-    firstLoop = false;
-  }
-  float angle = euler.x() - startAngle;
-  if (angle > 180) {
-    angle = 0; 
-  }
-  Serial.print("Angle: ");
-  Serial.println(angle);
-  serialVal = angle * 4 + 1500;
-  Serial.print("serialVal: ");
-  Serial.println(serialVal);
-  Serial.println("");
-  if (serialVal != 0) 
-  esc.writeMicroseconds(serialVal);
-  delay(20);
+  sensors_event_t event; 
+  double angle = euler.y();
+  return euler.y() - angleOffset;
+}
+
+int getEscValFromAngle(double angle) {
+    const int scalingValue = 1000;
+    const int minESC = 1400;
+    const int maxESC = 1600;
+    int scaledAngle = (int)(angle * scalingValue);
+    int escValue = map(scaledAngle, -angleBound * scalingValue, angleBound * scalingValue, minESC, maxESC);
+    return escValue;
+}
+
+void printErrorAndExit(String message) {
+    Serial.println(message);
+    while(true);
 }
   
